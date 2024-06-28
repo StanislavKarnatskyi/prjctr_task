0c08e83495f3dbb64e351603e555db72cd97e16f
Stanislav Karnatskyi <karnatskyiss@gmail.com>
2024-06-28 10:05:23 +0300

    Lecture_10

--- lecture_10/lecture_10_task_4/highest_score.py
+++ lecture_10/lecture_10_task_4/highest_score.py
@@ -1,10 +1,26 @@
 import csv
 
 list_ = []
+highest_list = []
 
 with open('../lecture_10_task_3/game_result/result.csv', 'r') as file:
     text = csv.reader(file)
     next(text)
     for row in text:
         list_.append(row)
-print(list_)
+list_sorted = sorted(list_, key=lambda x: int(x[1]), reverse=True)
+
+with open('highest_score/sorted_score.csv', 'w') as file:
+    text = csv.writer(file)
+    for i in range(len(list_sorted)):
+        text.writerow(list_sorted[i])
+
+with open('highest_score/sorted_score.csv', 'r') as file:
+    with open('highest_score/highest_score.csv', 'w') as file1:
+        text = csv.reader(file)
+        text1 = csv.writer(file1)
+        for row in text:
+            highest_list.append(row)
+        for i in range(5):
+            text1.writerow(highest_list[i])
+
