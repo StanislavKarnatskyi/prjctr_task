c3dc255f9afb322c131297395634e6e89e208890
Karnatskyi Stanislav <karnatskyiss@gmail.com>
2024-07-29 02:16:27 +0300

    test and task 4 from lecture 14

---
+++ lecture_14/task_3.py
@@ -0,0 +1,78 @@
+class Account:
+    def __init__(self, balance, account_number):
+        self._balance = balance
+        self._account_number = account_number
+
+    @classmethod
+    def create_account(cls, account_number):
+        return cls(0.0, account_number)
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+        else:
+            raise ValueError('Amount must be positive')
+
+    def withdraw(self, amount):
+        if amount > 0:
+            self._balance -= amount
+        else:
+            raise ValueError('Amount must be positive')
+
+    def get_balance(self):
+        return self._balance
+
+    def get_account_number(self):
+        return self._account_number
+
+    def __str__(self):
+        return f'Account number: {self._account_number}, balance: {self._balance}'
+
+
+
+class SavingsAccount(Account):
+    def __init__(self, balance, account_number, interest):
+        super().__init__(balance, account_number)
+        self.interest = interest
+
+    def add_interest(self):
+        self._balance = self._balance + (self._balance * (self.interest / 100))
+
+    def __str__(self):
+        return f'Account number: {self._account_number}, balance: {self._balance:.2f}'
+
+
+
+class CurrentAccount(Account):
+    def __init__(self, balance, account_number, overdraft):
+        super().__init__(balance, account_number)
+        self.overdraft = overdraft
+
+    def check_over(self, minus):
+        if (self._balance - minus) < -self.overdraft:
+            raise ValueError(f'Denied. {self._account_number} limit overdraft.')
+        else:
+            self._balance -= minus
+
+    def __str__(self):
+        return f'Account number: {self._account_number}, balance: {self._balance:.2f}'
+
+
+
+class Bank:
+    def __init__(self):
+        self.accounts = []
+
+    def open_account(self, account):
+        self.accounts.append(account)
+
+    def remove_account_by_id(self, account_id):
+        self.accounts = [account for account in self.accounts if account.get_account_number() != account_id]
+
+    def update(self):
+        for account in self.accounts:
+            if isinstance(account, SavingsAccount):
+                account.add_interest()
+            elif isinstance(account, CurrentAccount):
+                account.check_over(150)
+
---
+++ lecture_18/test_lecture_14.py
@@ -0,0 +1,89 @@
+from lecture_14.task_3 import Account, SavingsAccount, CurrentAccount, Bank
+import pytest
+
+
+@pytest.mark.parametrize(
+    "account_number, expected_number ,expected_balance, deposit, expected_result, withdraw, expected_withdraw", [
+        ("123", "123", 0, 123, 123, 10, 113),
+        (123, 123, 0, 406, 406, 300, 106),
+        ('SAA123', 'SAA123', 0, 134223, 134223, 134223, 0),
+        ('231SAA', '231SAA', 0, 12, 12, 13, -1),
+        ('saa', 'saa', 0, 0, False, 0, None),
+        ('/:$#', '/:$#', 0, -1, False, 0, None)
+    ])
+def test_account_functionality(account_number, expected_number, expected_balance, deposit, expected_result, withdraw,
+                               expected_withdraw):
+    test_account = Account.create_account(account_number)
+    assert test_account.get_account_number() == expected_number
+    assert test_account.get_balance() == expected_balance
+    if not expected_result:
+        with pytest.raises(ValueError):
+            test_account.deposit(deposit)
+    else:
+        test_account.deposit(deposit)
+        assert test_account.get_balance() == expected_result
+    if expected_withdraw == None:
+        with pytest.raises(ValueError):
+            test_account.withdraw(withdraw)
+    else:
+        test_account.withdraw(withdraw)
+        assert test_account.get_balance() == expected_withdraw
+
+
+# Test SavingsAccount and CurrentAccount
+
+@pytest.mark.parametrize("balance, account_number, interest, expected_interest", [
+    (100, 'sam', 15, 115),
+    (1000, '123', 1, 1010.00),
+    (0, '45', 3, 0),
+    (5, 'p1233', -2, 4.90),
+    (5000, 'perry', 30, 6500.00),
+    (30, 1222, 20, 36),
+])
+def test_savings_account(balance, account_number, interest, expected_interest):
+    test_account = SavingsAccount(balance, account_number, interest)
+    assert test_account.get_balance() == balance
+    assert test_account.get_account_number() == account_number
+    test_account.add_interest()
+    assert test_account.get_balance() == expected_interest
+
+
+@pytest.mark.parametrize("balance, account_number, overdraft, withdraw, expected_balance", [
+    (100, 'sam', 100, 200, -100),
+    (1000, '123', 300, 100, 900),
+    (0, '45', 0, 1000, False),
+])
+def test_savings_account(balance, account_number, overdraft, withdraw, expected_balance):
+    test_account = CurrentAccount(balance, account_number, overdraft)
+    assert test_account.get_balance() == balance
+    if expected_balance == False:
+        with pytest.raises(ValueError):
+            test_account.check_over(withdraw)
+    else:
+        test_account.check_over(withdraw)
+        assert test_account.get_balance() == expected_balance
+
+
+# Test Bank
+
+@pytest.mark.parametrize("balance, account_number, interest, expected_interest", [
+    (100, 'sam', 15, 115),
+    (1000, '123', 1, 1010.00),
+    (0, '45', 3, 0),
+])
+def test_bank_saving(balance, account_number, interest, expected_interest):
+    test_account = SavingsAccount(balance, account_number, interest)
+    bank = Bank()
+    bank.open_account(test_account)
+    assert len(bank.accounts) == 1
+    for account in bank.accounts:
+        bank.remove_account_by_id(account.get_account_number())
+    assert len(bank.accounts) == 0
+
+
+@pytest.mark.parametrize("balance, account_number, overdraft, expected_balance", [
+    (150, 'sam', 0, -150),
+    (1000, '123', 300, 850),
+])
+def test_bank_current(balance, account_number, overdraft, expected_balance):
+    pass
